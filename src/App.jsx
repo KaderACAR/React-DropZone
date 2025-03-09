7db6cf450b49b18766b29c675b57291b87bce555
Kader Acar <acarkader67@gmail.com>
2025-03-10 01:20:01 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './index.css'
+import DropzoneComponent from './DropzoneComponent'
+
+function App() {
+
+  return (
+    <>
+      <div >
+      <DropzoneComponent />
+      </div>
+      
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/DropzoneComponent.jsx
@@ -0,0 +1,77 @@
+import { useDropzone } from 'react-dropzone'
+import { IoMdCloudUpload } from 'react-icons/io'
+import { MdDelete } from 'react-icons/md'
+import { useState } from "react";
+
+
+const DropzoneComponent = () => {
+  
+  const [files, setFiles] = useState([]);
+
+  const{ acceptedFiles, getRootProps, getInputProps, isDragActive} = useDropzone({
+    accept: {
+      'image/*': ['.jpg', '.jpeg', '.png'],
+      'text/html': ['.html', '.htm'],
+    },
+    maxSize: 5 * 1024 * 1024,
+
+    onDrop: (acceptedFiles) => {
+      setFiles([...files, ...acceptedFiles]);
+    },
+  });
+
+    const removeFile = (fileName) => {
+      setFiles(files.filter((file) => file.name !== fileName));
+    };
+    
+  return (    
+    <div className="flex flex-col items-center mt-5">
+    <h1 className='text-center m-5 my-10 text-2xl font-semibold text-[#501717]'
+    ><em className='text-[#ff5400] text-6xl'>React Dropzone</em></h1>
+      <div
+        {...getRootProps()}
+        className={`w-full max-w-xl border-2 border-dashed m-5 p-6 rounded-lg text-center cursor-pointer transition 
+        ${isDragActive ? "border-white-500 bg-white-100" : "border-[#ff6d00] bg-white"} 
+        hover:border-lime-500 hover:bg-lime transition`}
+      >
+        <input {...getInputProps()} />
+        
+        {isDragActive ? (
+          <p className='text-lg text-red-700'>Dosyaları buraya bırakın... </p>
+        ) : (
+          <div className="flex flex-col items-center">
+            <IoMdCloudUpload size="5rem" className='text-[#ff5400]' />
+            <p className='text-lg text-gray-700' >Sürükle bırak yapın veya dosya seçin</p>
+          </div>
+        )}
+        
+        <em className='block mt-2 text-sm text-gray-500' >(Yalnızca *.jpeg, *.png ve *.html dosyaları)</em>
+      </div>
+      
+      {acceptedFiles.length > 0 && (
+        <div className='mt-0  text-[#ff5400]'>
+          <h4 className='text-lg font-semibold'>Yüklenen Dosyalar:</h4>
+          <ul className='mt-2 space-y-1'>
+            {files.map(file => (
+              <li key={file.name} className='text-lg flex items-center gap-3'>
+              <span>{file.name} - {Math.round(file.size / 1024)} KB</span>
+                <button
+                  onClick={() => removeFile(file.name)}
+                  className="flex flex-col text-red-400 hover:text-red-700 transition"
+                >
+                  <MdDelete size={20} />
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+
+   
+  
+  );
+};
+    
+
+export default DropzoneComponent;
